d06e8319ddf3a0a01e42714027782642eb4850e9
Gilberto R. Silva <gilberto.rodrigues07@aluno.ifce.edu.br>
2025-12-12 16:04:57 -0300

    Resolvendo erro do arquivo Style.css para style.css

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import time
 from scripts import predict
-
+# Aplicação Streamlit para predição de risco de depressão
 with open("Style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
